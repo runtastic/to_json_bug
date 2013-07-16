9df4f9018ec5a543bd660976e180562a84f9dad6
lister <simon@lisuna.org>
2013-07-16 16:56:15 +0200

    Update bug.rb

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,9 +1,12 @@
 class Bug < ActiveRecord::Base
   # choose one ;)
   
+  # no fix yet, rename table ;)
 #  self.table_name = "bugs" 
 
+  # fix by using attr_accessible or strong_parameters
 #  attr_protected :name
 
+  # fix by using lambda syntax
 #  scope :buggy, where(name: "bug")
 end
